af2a3f7aa8a6929f3aff6c92adecfcef6cc31e9a
Ehi001 <ehidiamen1@gmail.com>
2025-04-02 21:20:54 -0600

    Data Loading and DAG Script Completed

---
+++ Loading.py
@@ -0,0 +1,41 @@
+# Import necessary libraries
+import pandas as pd
+from azure.storage.blob import BlobServiceClient
+from dotenv import load_dotenv
+import os
+
+def run_loading():
+    # Loading the dataset
+    data = pd.read_csv('cleaneddata.csv')
+    products = pd.read_csv('products.csv.csv')
+    customers = pd.read_csv('customers.csv')
+    staff = pd.read_csv('staff.csv')
+    transactions = pd.read_csv('transactions.csv')
+
+    # Load environment variables from the .env files
+    load_dotenv()
+
+    connection_str = os.getenv('AZURE_CONNECTION_STRING_VALUE')
+    container_name = os.getenv('CONTAINER_NAME')
+
+    # Create a BlobServiceClient object
+    blob_service_object = BlobServiceClient.from_connection_string(connection_str)
+    container_client = blob_service_object.get_container_client(container_name)
+
+    #Load data to Azure Blob Storage
+    files = [
+        (data, 'rawdata/cleaned_zipco_transaction_data.csv'),
+        (products, 'cleaneddata/productscsv'),
+        (customers, 'cleaneddata/customers.csv'),
+        (staff, 'cleaneddata/staff.csv'),
+        (transactions, 'cleaneddata/transactions.csv')
+
+    ]
+
+    for file, blob_name in files:
+        blob_client = container_client.get_blob_client(blob_name)
+        output = file.to_csv(index = False)
+        blob_client.upload_blob(output, overwrite = True)
+        print(f'{blob_name} loaded into Azure Blob Storage')
+
+
---
+++ Transformation.py
@@ -0,0 +1,61 @@
+import pandas as pd
+
+def run_transformation():
+
+    # Remove Duplicates
+    data = pd.read_csv(r'zipco_transaction.csv')
+    data.drop_duplicates(inplace = True)
+
+    # Handle missing numeric values by filling with the mean or median
+    numeric_columns = data.select_dtypes(include = ['float64', 'int64']).columns
+    for col in numeric_columns:
+        data.fillna({col : data[col].mean()}, inplace = True)
+
+    # Handle missing string values by filling with 'Unknown'
+    string_columns = data.select_dtypes(include = ['object']).columns
+    for col in string_columns:
+        data.fillna({col : 'Unknown'}, inplace = True)
+
+    # Assigning the right datatype to the date column
+    data['Date'] = pd.to_datetime(data['Date'])
+
+    # Creating the dimension tables
+
+    # Create the product table
+    products = data[['ProductName']].copy().drop_duplicates().reset_index(drop = True)
+    products.index.name = 'ProductID'
+    products = products.reset_index()
+
+    # Create the customer table
+    customers = data[['CustomerName', 'CustomerAddress', 'Customer_PhoneNumber', 'CustomerEmail']].copy().drop_duplicates().reset_index(drop = True)
+    customers.index.name = 'CustomerID'
+    customers = customers.reset_index()
+
+
+    # Create the staff table
+    staff = data[['Staff_Name', 'Staff_Email', 'StaffPerformanceRating']].copy().drop_duplicates().reset_index(drop = True)
+    staff.index.name = 'StaffID'
+    staff = staff.reset_index()
+
+    # Creating the fact table
+
+    # Create Transactions table
+    transactions = data.merge(products, on = ['ProductName'], how = 'left')\
+        .merge(customers, on = ['CustomerName', 'CustomerAddress', 'Customer_PhoneNumber', 'CustomerEmail'], how = 'left')\
+            .merge(staff, on = ['Staff_Name', 'Staff_Email', 'StaffPerformanceRating'], how = 'left')
+
+    transactions = transactions[['Date', 'ProductID', 'UnitPrice', 'Quantity', 'StoreLocation',
+       'PaymentType', 'PromotionApplied', 'Weather', 'Temperature', 'CustomerFeedback', 'DeliveryTime_min',
+       'OrderType', 'CustomerID', 'StaffID', 'DayOfWeek', 'TotalSales']]
+
+    transactions.index.name = 'Transaction_ID'
+    transactions = transactions.reset_index()
+
+    # save cleaned data as csv files
+    data.to_csv('clean_data.csv', index = False)
+    products.to_csv('products.csv', index = False)
+    customers.to_csv('customers.csv', index = False)
+    staff.to_csv('staff.csv', index = False)
+    transactions.to_csv('transactions.csv', index = False)
+
+    print('Data cleaning and transformation completed successfully')
